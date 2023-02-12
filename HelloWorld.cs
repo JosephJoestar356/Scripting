0ef41af6752b002979751f8d7f7a821700569c25
JosephJoestar356 <125181438+JosephJoestar356@users.noreply.github.com>
2023-02-12 11:17:58 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update privet pupsik
+    void Start()
+    {
+        //Вот так сожно выводить символы
+        print("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
