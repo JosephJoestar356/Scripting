d0417ee6ddb77f38e453731aa841fb2d46ecdd7a
JosephJoestar356 <125181438+JosephJoestar356@users.noreply.github.com>
2023-02-12 11:20:37 +0300

    eto moy perviy script
    pablo amogus 777 228 pups bebrik

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update privet pupsik
+    void Start()
+    {
+        //vot tak mosgno vivodit simvoli
+        print("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
